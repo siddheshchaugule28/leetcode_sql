bd3b1810e3a08787535952e51617d710b1abfc1e
Siddhesh Chaugule <130450558+siddheshchaugule28@users.noreply.github.com>
2024-03-19 08:59:51 +0530

    Time: 1912 ms (47.71%), Space: 0B (100.00%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+WITH CTE AS (
+    SELECT 
+        turn, person_name, weight,
+        SUM(weight) OVER(ORDER BY turn ASC) AS tot_weight 
+    FROM Queue
+    ORDER BY turn
+)
+SELECT person_name
+FROM Queue q
+WHERE q.turn = (SELECT MAX(turn) FROM CTE WHERE tot_weight <= 1000);
